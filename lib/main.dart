d38524cb000fcd1a97cdadadfb90db5f8a90b1e5
Emeka Jideije <jideije.emeka@gmail.com>
2022-10-13 11:38:49 +0100

    Initialize Project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:life_advice/views/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
