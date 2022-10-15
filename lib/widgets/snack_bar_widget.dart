091bc9d223bd97d71e6b5f403fc4c40aec85b0e9
Emeka Jideije <jideije.emeka@gmail.com>
2022-10-15 02:37:41 +0100

    Design custom snack bar

---
+++ lib/widgets/snack_bar_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:life_advice/app_text_styles.dart';
+
+SnackBar snackBar({required String message}) {
+  return SnackBar
+    (elevation: 5, padding: const EdgeInsets.all(25),
+      margin:  const EdgeInsets.all(25),
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: const BorderSide(color: Colors.blue, width: 2.5)
+      ),
+      content: Text(message, style: AppTextStyles.normalTextStyle(
+          color: Colors.white),));
+}
