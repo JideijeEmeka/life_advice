7d3228e74a6d64269bef9a7d4ac19f8d30f3b6fe
Emeka Jideije <jideije.emeka@gmail.com>
2023-01-06 21:24:36 +0100

    Add new text font

--- lib/app_text_styles.dart
+++ lib/app_text_styles.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
   static TextStyle normalTextStyle({required Color color}) {
-    return TextStyle(
-        fontSize: 17, color: color
+    return GoogleFonts.poppins(
+      textStyle: TextStyle(
+          fontSize: 17, color: color
+      )
     );
   }
   static TextStyle adviceTextStyle = const TextStyle(
