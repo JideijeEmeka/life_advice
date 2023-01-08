12405445b725cdeee07a9bb1445a410b3216fe3c
Emeka Jideije <jideije.emeka@gmail.com>
2023-01-08 19:02:48 +0100

    Redesign the drawer view

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -27,16 +27,17 @@ class _AppDrawerState extends StateMVC<AppDrawer> {
         children: [
           DrawerHeader(
               decoration: const BoxDecoration(
-                  color: Colors.blue
+                  color: Colors.black
               ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const CircleAvatar(
                     radius: 30,
                     backgroundColor: Colors.white,
                     child: Text('GA', style: TextStyle(
-                        fontSize: 24, color: Colors.blue
+                        fontSize: 24, color: Colors.black
                     ),),
                   ),
                   Padding(
@@ -44,8 +45,8 @@ class _AppDrawerState extends StateMVC<AppDrawer> {
                     child: Text('Get Advice', style: AppTextStyles.normalTextStyle(
                         color: Colors.white)),
                   ),
-                  Text('jideije.emeka@gmail.com', style: AppTextStyles.normalTextStyle(
-                      color: Colors.white))
+                  // Text('jideije.emeka@gmail.com', style: AppTextStyles.normalTextStyle(
+                  //     color: Colors.white))
                 ],)),
           Padding(
             padding: const EdgeInsets.only(right: 40, top: 10),
@@ -55,15 +56,13 @@ class _AppDrawerState extends StateMVC<AppDrawer> {
                   selected = true;
                   index = 1;
                 });
-                Navigator.pop(context);
                 Future.delayed(const Duration(milliseconds: 100), () {
                   Navigator.push(context, MaterialPageRoute(builder: (_)
-                  => FavoriteView(favAdviceList: con.favAdviceList,)));
+                  => FavoriteView(favAdviceList: con.favAdviceList, drawerIndex: 1,)));
                 });
               },
               selected: selected,
-              selectedTileColor: index == 1 ? Colors.blue : null,
-              tileColor: Colors.blue,
+              selectedTileColor: index == 1 ? Colors.black : null,
               leading: Icon(Icons.favorite_border_outlined,
                 color: index == 1 ? Colors.white : Colors.black,),
               title: Text('Favorites', style: AppTextStyles.normalTextStyle(
@@ -85,7 +84,7 @@ class _AppDrawerState extends StateMVC<AppDrawer> {
                 });
               },
               selected: selected,
-              selectedTileColor: index == 2 ? Colors.blue : null,
+              selectedTileColor: index == 2 ? Colors.black : null,
               leading: Icon(Icons.settings,
                 color: index == 2 ? Colors.white : Colors.black,),
               title: Text('Settings', style: AppTextStyles.normalTextStyle(
@@ -107,7 +106,7 @@ class _AppDrawerState extends StateMVC<AppDrawer> {
                 });
               },
               selected: selected,
-              selectedTileColor: index == 3 ? Colors.blue : null,
+              selectedTileColor: index == 3 ? Colors.black : null,
               leading: Icon(Icons.account_circle_outlined,
                 color: index == 3 ? Colors.white : Colors.black,),
               title: Text('Account', style: AppTextStyles.normalTextStyle(
